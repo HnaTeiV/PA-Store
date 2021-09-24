b12b61b33dec5d03c03d2d876f85e8d7dc2d0a71
PhongDoan69 <87539498+PhongDoan69@users.noreply.github.com>
2021-09-25 00:06:37 +0700

    update js + fix h3

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@ function init(){
     }
 }
 function search(){
-        var k=document.getElementById("kw")
-        if(k!=null){
-            k=k.value
-            var product=document.querySelectorAll(".productTitle")
-            for(var i=0;i<product.length;i++){
-            var h3=product[i].getElementsByTagName("h3")[0].innerText
-                if(h3.indexOf(k)>=0)
-                    product[i].style.color="red"
-            }
+var k=document.getElementById("kw")
+if(k!=null){
+    k=k.value
+    var product=document.querySelectorAll(".productTitle")
+    for(var i=0;i<product.length;i++){
+    var h3=product[i].getElementsByTagName("h3")[0].innerText
+        if(h3.indexOf(k)>=0)
+            product[i].style.color="red"
+    }
     setTimeout(function() {
         var product = document.querySelectorAll(".productTitle")
             for (var i = 0; i < product.length; i++)
@@ -39,35 +39,40 @@ $(document).ready(function(){
         $("#goToTop").hide("Slow")
        })
        $("#goToTop").click(function(){
-           $("html,body").animate({scrollTop:0},1000)
+        $("html,body").animate({scrollTop:0},1000)
     })
-<<<<<<< HEAD
+    
+    $("#goToFooter").click(function(){
+        $("html,body").animate({scrollTop:2000},1000)
+    })
+    $("#btnButton").click(function(){
+        $("html,body").animate({scrollTop:960},1000)
+    })
 }
 )
 
+
 function buy(){
-        const modal  = document.querySelector('.modal')
-        const closeModal = document.querySelector('.modal-close')
-        const modalContainer = document.querySelector('.js-modal-container')
-        
-        modal.classList.add('open')
-        function closeBuyTickets(){
-            modal.classList.remove('open')
-        }
-        closeModal.addEventListener('click',closeBuyTickets)
-        modal.addEventListener('click',closeBuyTickets)
-        modalContainer.addEventListener('click',function(event){
-            event.stopPropagation()
-        })
-}
-=======
-   $("nav ul.types li a").click(function(){
-       var link=$(this).attr("href")
-       $("html, body").animate({
-           scrollTop: $(link).prop("offsetTop")+"px"
-       },1000)
-   })
+    const modal  = document.querySelector('.modal')
+    const closeModal = document.querySelector('.modal-close')
+    const modalContainer = document.querySelector('.js-modal-container')
     
-   
+    modal.classList.add('open')
+    function closeBuyTickets(){
+        modal.classList.remove('open')
+    }
+    closeModal.addEventListener('click',closeBuyTickets)
+    modal.addEventListener('click',closeBuyTickets)
+    modalContainer.addEventListener('click',function(event){
+        event.stopPropagation()
+    })
+}
+$("nav ul.types li a").click(function(){
+    var link=$(this).attr("href")
+    $("html, body").animate({
+        scrollTop: $(link).prop("offsetTop")+"px"
+    },1000)
 })
->>>>>>> 619f9b494b0a4881dad157dd958cfd12f1ebf863
+
+
+
